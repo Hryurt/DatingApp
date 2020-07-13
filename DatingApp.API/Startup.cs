ed34ff294dfd9d07cc21c9bb68f917756e90cc35
hryurt <h.ramazan.yurt@gmail.com>
2020-07-13 23:11:56 +0300

    Somewhere in section 9

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -35,9 +35,10 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
